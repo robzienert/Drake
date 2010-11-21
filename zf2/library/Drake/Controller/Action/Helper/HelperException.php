c61552bb4259713c539d2de12098d22e25c5a3de
Rob Zienert <rob@robzienert.com>
2010-11-21 18:56:27 -0500

    adding missing helper exception; renaming model files

---
+++ zf2/library/Drake/Controller/Action/Helper/HelperException.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Drake Framework
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the BSD License that is bundled with this
+ * package in the file LICENSE. It is also available through the world-wide-web
+ * at this URL: http://github.com/robzienert/Drake/blob/develop/LICENSE
+ *
+ * @category    Drake
+ * @package     Drake_Controller
+ * @subpackage  Helpers
+ * @copyright   Copyright (c) 2008-2010 Rob Zienert (http://robzienert.com)
+ * @license     http://github.com/robzienert/Drake/blob/develop/LICENSE New BSD
+ */
+
+/**
+ * @namespace
+ */
+namespace Drake\Controller\Action\Helper;
+
+/**
+ * @category    Drake
+ * @package     Drake_Controller
+ * @subpackage  Helpers
+ * @copyright   Copyright (c) 2008-2010 Rob Zienert (http://robzienert.com)
+ * @license     http://github.com/robzienert/Drake/blob/develop/LICENSE New BSD
+ */
+class HelperException extends \RuntimeException implements Exception
+{
+}
